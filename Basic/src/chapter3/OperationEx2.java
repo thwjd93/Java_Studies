616db58fe6d4910844ab3ec8a573ea3bb6ab243e
thwjd93 <imsjim@kakao.com>
2022-03-18 00:07:54 +0900

    Basic

--- Basic/src/chapter3/OperationEx2.java
+++ Basic/src/chapter3/OperationEx2.java
@@ -8,7 +8,7 @@ public class OperationEx2 {
 		System.out.println(num++); //출력이 되는 statement 후에 ++ 실행
 		System.out.println(num);
 				
-		System.out.println(3>5); 
+		System.out.println(3>5); //관계 연산자 
 		int num1 = 10;
 		int num2 = 5;
 		boolean flag = num1 > num2;
