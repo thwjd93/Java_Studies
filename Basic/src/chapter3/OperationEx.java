fc99dbfcc5889f3ca5dc597dcbbff2751c4650f1
thwjd93 <imsjim@kakao.com>
2022-03-17 23:37:29 +0900

    Basic

---
+++ Basic/src/chapter3/OperationEx.java
@@ -0,0 +1,18 @@
+package chapter3;
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 24;
+		System.out.println(age);
+		
+		int num = 10;
+		int num2 = -num;
+		System.out.println(num);
+		System.out.println(num2);
+		
+		
+	}
+
+}
---
+++ Basic/src/chapter3/OperationEx2.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(++num); //출력이 되는 statement 전에 ++ 실행
+		System.out.println(num++); //출력이 되는 statement 후에 ++ 실행
+		System.out.println(num);
+				
+	}
+
+}
